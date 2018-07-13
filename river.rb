adf9f2369e862751bc1f5c27f9133157fbe026a9
hpaulini <helenpaulini@gmail.com>
2018-07-13 15:08:30 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -4,8 +4,11 @@ class River
   
   attr_writer :name, :length, :countries, :discharge
   
-  def initialize
-    
+  def initialize (name, length, countries, discharge)
+    @name = name
+    @length  = length
+    @countries = countries
+    @discharge = discharge
   end
   
   def flood
